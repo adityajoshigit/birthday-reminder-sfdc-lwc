a190b1c93d21fdba6c738a15272db3e7aeffd58f
adityajoshigit <adityasjoshi96@gmail.com>
2023-01-04 02:03:04 -0500

    date timezone issue fixed - need to remove console.log()

--- force-app/main/default/lwc/bdayPerson/bdayPerson.js
+++ force-app/main/default/lwc/bdayPerson/bdayPerson.js
@@ -14,18 +14,35 @@ export default class BdayPerson extends LightningElement {
     }
 
     get displayAge() {
-        return this.calculateAge(new Date(this.bdayIndividualRecord.Birthdate)) + ' years';
+        if (this.bdayIndividualRecord) {
+            let parts = this.bdayIndividualRecord.Birthdate.split('-');
+            let bdate = new Date();
+            console.log(parts);
+            console.log(bdate);
+            bdate.setDate(parseInt(parts[2]));
+            bdate.setMonth(parseInt(parts[1])-1);
+            bdate.setFullYear(parseInt(parts[0]));
+            console.log(bdate);
+            return this.calculateAge(bdate) + ' years';
+        } else {
+            return '-NA-';
+        }
     }
 
     calculateAge(birthdate) {
         let age = 0;
         console.log(birthdate);
         if (birthdate) {
+            console.log(
+                ( todayDate.getDate() + " == " + birthdate.getDate() )
+            +"\n"+  ( todayDate.getMonth() + " == " + birthdate.getMonth() )
+            );
             if (
                 ( todayDate.getDate() === birthdate.getDate() )
             &&  ( todayDate.getMonth() === birthdate.getMonth() )
             ) {
-                return todayDate.getFullYear() - birthdate.getFullYear();
+                console.log("here --- " + birthdate);
+                age = todayDate.getFullYear() - birthdate.getFullYear();
             }
         }
         return age;
