fbb46023482cc975934d234881534e01e1e22146
adityajoshigit <adityasjoshi96@gmail.com>
2023-01-04 01:11:02 -0500

    age calculation done and some stylinf for icon

--- force-app/main/default/lwc/birthdayReminder/birthdayReminder.js
+++ force-app/main/default/lwc/birthdayReminder/birthdayReminder.js
@@ -1,50 +1,18 @@
-import { LightningElement, wire, track } from 'lwc';
+import { LightningElement, wire, track, api } from 'lwc';
 import getBirthdayContacts from '@salesforce/apex/BirthdayReminderUtility.getBirthdayContacts';
 
+const todayDate = new Date();
+
 export default class BirthdayReminder extends LightningElement {
-    header = 'Today\'s Birthdays!';
-    @track 
-    iconName = 'action:user';
+    @api baseBirthDate;
+    
+    @track iconName = 'action:user';
 
-
-    bdayContacts = [
-        {
-            FirstName: 'Aditya',
-            LastName: 'Joshi',
-            Birthday: '1996-11-24',
-            Id: '1'
-        },
-        {
-            FirstName: 'Saurabh',
-            LastName: 'Kalelkar',
-            Birthday: '1997-03-24',
-            Id: '2'
-        },
-        {
-            FirstName: 'Abhijeet',
-            LastName: 'Patwardhan',
-            Birthday: '2000-11-12',
-            Id: '3'
-        },
-        {
-            FirstName: 'Anagha',
-            LastName: 'Patwardhan',
-            Birthday: '1995-12-02',
-            Id: '4'
-        },
-        {
-            FirstName: 'Mihir',
-            LastName: 'Paradkar',
-            Birthday: '1996-12-03',
-            Id: '5'
-        },
-        {
-            FirstName: 'Rutuja',
-            LastName: 'Gadgil',
-            Birthday: '1998-1-01',
-            Id: '6'
-        }
-    ];
+    get header() {
+        return (
+            (!this.baseBirthDate || this.baseBirthDate.getTime() === todayDate.getTime()) ? 'Today\'s Birthdays' : ('Birthdays on ' + baseBirthDate)
+        );
+    }
 
     @wire(getBirthdayContacts, {bdateString: ''})
     todayBdayContacts;
@@ -56,14 +24,17 @@ export default class BirthdayReminder extends LightningElement {
     /**
      * calculates age based on the birthdate sent in the form of yyyy-mm-dd
      * @param {string} birthdate 
+     * @returns {number} age value
      */
     calculateAge(birthdate) {
+        let age = 0;
+        
         if (birthdate) {
-            let parts = birthdate.split('-');
-            if (parts && parts.length === 3) {
-                
-            }   
+            if (birthdate.getTime() === todayDate.getTime()) {
+                return todayDate.getFullYear() - birthdate.getFullYear();
+            }
         }
+        return age;
     }
 
     showIcon() {
@@ -73,4 +44,5 @@ export default class BirthdayReminder extends LightningElement {
     hideIcon() {
         this.iconName = 'action:approval';
     }
+
 }
